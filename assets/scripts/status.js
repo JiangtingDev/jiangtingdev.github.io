4fe46433f5f2010a4e68dcf3f53500d4aa254048
JiangtingDev <jiangtingssd2019@yeah.net>
2024-02-18 22:38:08 +0800

    优化Switch样式

--- assets/scripts/status.js
+++ assets/scripts/status.js
@@ -1,17 +1,4 @@
-const switchs = {}
-
-function onSwitch(id) {
-    let swich = document.getElementById(id);
-    let childs = swich.childNodes
-    let content = childs[1].childNodes
-    if (childs[0]) {
-        childs[0].style.transform = `translate(${switchs[id]?"0":"30px"}, -50%)`; 
-        content[0].style.transform = `translate(${switchs[id]?"-30px":"6px"}, 0)`;
-        content[1].style.transform = `translate(${switchs[id]?"0":"36px"}, 0)`; 
-        swich.style.background = switchs[id]?"#FF9C7D":"#19D09F";
-        switchs[id] = !switchs[id]; 
-    } 
-}
+const refreshSwitch = document.getElementById("switch")
 
 function timeoutFetch(timeOut = 1000) {
     return function (url, options) {
@@ -30,7 +17,7 @@ function timeoutFetch(timeOut = 1000) {
 }
 
 function fetchData() {
-    timeoutFetch(3000)("https://api.jiangting.top/status/?")
+    timeoutFetch(4000)("https://api.jiangting.top/status/?")
         .then(response => response.json())
         .then(data => {
             document.getElementById("data").innerHTML = `游玩地址：mc.jiangting.top<br/>
@@ -40,7 +27,7 @@ function fetchData() {
             数据时间：${data.time}`
         })
         .catch(error => {
-            console.error("Error fetching data:", error);
+            //console.error("Loding err:", error);
         });
 }
 
@@ -50,4 +37,4 @@ function getTimeStr() {
 }
 
 fetchData()
-setInterval(() => {if (switchs.auto) fetchData()}, 5000)
+setInterval(() => {if (refreshSwitch.checked) fetchData()}, 5000)
